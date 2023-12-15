7a0235e0ba0ae7195b07a5f8950630bac2474e13
uggoth <panda@sidmouthrobots.co.uk>
2023-12-15 12:33:47 +0000

    GENERALISED AX12 TEST

--- test_06_AX12_A_Basic_V01.py
+++ test_06_AX12_A_Basic_V01.py
@@ -1,15 +1,25 @@
 module_name = 'test_06_AX12_A_Basic_V01.py'
-
 print (module_name, 'starting')
 
 from pyax12.connection import Connection
 
-ax12_connection = Connection(port='/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0', baudrate=1000000)
+#  To obtain the port name use:    ls /dev/serial/by-path
+port_name = 'platform-3f980000.usb-usb-0:1.1.3:1.0-port0'
+ax12_connection = Connection(
+    port='/dev/serial/by-path/' + port_name,
+    baudrate=1000000)
 
 dynamixel_id = 16
 
-is_available = ax12_connection.ping(dynamixel_id)
+ids_available = ax12_connection.scan()
 
-print (is_available)
+for id in ids_available:
+    result = ax12_connection.ping(id)
+    if result:
+        print (id, 'Available')
+    else:
+        print (id, '*** NO RESPONSE ***')
 
 ax12_connection.close()
+
+print (module_name, 'finished')
